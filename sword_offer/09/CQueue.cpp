46f00f646f2b0cf31989fe9a5556b81010e0cc56
siwen <suwei@epaynfc.com>
2021-02-07 00:04:56 +0800

    用两个栈实现队列c++代码

---
+++ sword_offer/09/CQueue.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+class CQueue {
+public:
+    stack<int> in_stack;
+    stack<int> out_stack;
+    CQueue() {}
+
+    void appendTail(int value) {
+        in_stack.push(value);
+    }
+
+    int deleteHead() {
+        if (!out_stack.empty()) {
+            int top = out_stack.top();
+            out_stack.pop();
+            return top;
+        } else {
+            while (in_stack.size() > 1) {
+                int top = in_stack.top();
+                in_stack.pop();
+                out_stack.push(top);
+            }
+            if (in_stack.size() == 1) {
+                int top = in_stack.top();
+                in_stack.pop();
+                return top;
+            } else {
+                return -1;
+            }
+        }
+    }
+};
